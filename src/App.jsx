9f2fc3514f4295819906a8116039964105692c13
Tomas.K <tomas.konopackis@gmail.com>
2024-08-23 14:06:13 +0300

    update section six

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { SectionFive } from './components/SectionFive.jsx';
 import { SectionFour } from './components/SectionFour.jsx';
 import { SectionOne } from './components/SectionOne.jsx';
+import { SectionSix } from './components/SectionSix.jsx';
 import { SectionThree } from './components/SectionThree.jsx';
 import { SectionTwo } from './components/SectionTwo.jsx';
 
@@ -14,6 +15,7 @@ function App() {
       <SectionThree/>
       <SectionFour/>
       <SectionFive/>
+      <SectionSix/>
     </>
   )
 }
---
+++ src/components/SectionSix.jsx
@@ -0,0 +1,45 @@
+import "./SectionSix.css";
+
+export const SectionSix = () => {
+
+    return (
+        <>
+        <section className="sectionSix">
+            <div>
+                <div>
+                    <a href="#">Questions? Contact us</a>
+                </div>
+                <div className="linksConatainer">
+                    <div>
+                        <a href="#">FAQ</a>
+                        <a href="#">Investor Relations</a>
+                        <a href="#">Privacy</a>
+                        <a href="#">Speed Test</a>
+                        <a href="#">Ad Choices</a>
+                    </div>
+                    <div>
+                        <a href="#">Help Center</a>
+                        <a href="#">Jobs</a>
+                        <a href="#">Cookie Preferences</a>
+                        <a href="#">Legal Guarantee</a>
+                    </div>
+                    <div>
+                        <a href="#">Account</a>
+                        <a href="#">Ways to Watch</a>
+                        <a href="#">Corporate Information</a>
+                        <a href="#">Legal Notices</a>
+                    </div>
+                    <div>
+                        <a href="#">Media Center</a>
+                        <a href="#">Terms of Use</a>
+                        <a href="#">Contact Us</a>
+                        <a href="#">Only on Netflix</a>
+                    </div>
+                </div>
+            </div>
+            <span className='language'>૱ English ▼</span>
+            <p>Netflix Lithuania</p>
+        </section>
+        </>
+    );
+};
