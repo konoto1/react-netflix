564145b93e8aaa97cff4e180868eb10055cca43e
Tomas.K <tomas.konopackis@gmail.com>
2024-08-23 09:49:49 +0300

    Initial commit netflix page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { SectionOne } from './components/SectionOne.jsx';
+
+function App() {
+
+  return (
+    <>
+      <SectionOne/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SectionOne.jsx
@@ -0,0 +1,11 @@
+import netflixBackground from '../../public/netflixBackground.jpg';
+
+export const SectionOne = () => {
+    return (
+        <>
+            <section className="sectionOne">
+                {/* <img src={netflixBackground} alt="" /> */}
+            </section>
+        </>
+    )
+}
